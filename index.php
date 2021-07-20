98dce98c12351d69e59b675ede4be06f372e027a
Arthur Henrique Wiebusch <arturh07@gmail.com>
2021-07-19 21:01:54 -0300

    chore: starting the project

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Course</title>
+  </head>
+  <body>
+    <?php echo "<h1>Hello World!</h1>"; ?>
+  </body>
+</html>
