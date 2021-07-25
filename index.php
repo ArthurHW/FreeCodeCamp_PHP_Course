15ff983e1bfcbbcec8850d293e8d17d4ead27c93
Arthur Henrique Wiebusch <arturh07@gmail.com>
2021-07-25 19:09:47 -0300

    chore: 1h:30min  de curso concluido

--- index.php
+++ index.php
@@ -43,7 +43,88 @@
     <h2>Working with numbers</h2>
     <?php
         $integerNumber = 40;
-        $floatNumber = $40.50;
+        $floatNumber = 40.50;
+        // Operations: +, *, -, /, %
+        $num = 10;
+        echo $num++; // increment
+        echo "<br>";
+        echo $num--; // decrement
+        echo "<br>";
+        echo $num += 25;
+        echo "<br>";
+        echo $num -= 10;
+        echo "<br>";
+        echo abs(-100); // absolute value
+        echo "<br>";
+        echo pow(2, 4); // power function
+        echo "<br>";
+        echo sqrt(144); // square root
+        echo "<br>";
+        echo max(2, 10); // returns the bigger value
+        echo "<br>";
+        echo min(2, 10); // return the smaller number
+        echo "<br>";
+        echo round(3.2); // rounds the number to the nearest integer
+        echo "<br>";
+        echo ceil(3.3); // round the number up
+        echo "<br>";
+        echo floor(3.9); // rounds the number down
+        echo "<br>";
      ?>
+     <h2>Getting User Input</h2>
+      <!-- <form class="" action="index.php" method="get">
+        <fieldset class="personal-data">
+          <label for="name-input">Name: </label>
+          <input type="text" name="name" id="name-input">
+          <label for="age-input">Age: </label>
+          <input type="number" name="age" id="age-input">
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form> -->
+      <br>
+      <?php
+        echo "Your name is: ";
+        echo $_GET["name"];
+        echo "<br>";
+        echo "Your age is: ";
+        echo $_GET["age"];
+       ?>
+       <h2>Building a basic calculator</h2>
+       <!-- <form action="index.php" method="GET">
+          <input type="number" name="first-number">
+          <br>
+          <input type="number" name="second-number">
+          <button type="submit">Calculate</button>
+       </form> -->
+       Answer:
+       <?php
+          echo $_GET["first-number"] + $_GET["second-number"];
+       ?>
+       <h2>Building a mad libs game</h2>
+       <!-- <form action="index.php" method="GET">
+         Color: <input type="text" name="color"> <br>
+         Plural Noun: <input type="text" name="pluralNoun"> <br>
+         Someone: <input type="text" name="someone"> <br>
+         <button type="submit">Submit</button>
+       </form> -->
+       <?php
+          $color = $_GET["color"];
+          $pluralNoun = $_GET["pluralNoun"];
+          $someone = $_GET["someone"];
+          echo "Roses are $color <br>";
+          echo "$pluralNoun are blue <br>";
+          echo "I love  $someone<br>";
+       ?>
+       <h2>URL parameters</h2>
+       <form action="index.php" method="GET">
+          Name: <input type="text" name="name"> <br>
+          <button type="submit">Submit</button>
+       </form>
+       <br><br>
+       <?php
+        echo $_GET["name"];
+        ?>
+
+
   </body>
 </html>
